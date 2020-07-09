426b9f35f0fa3722f880fb41dab60fcb50bec8e2
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-09 15:11:01 +0530

    wishlist component added

---
+++ src/components/wishlist/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../scss/index.scss";
+import Footer from "../common/footer";
+
+const Wishlist = () => {
+  return (
+    <div>
+      <h1>Hello Wishlist</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Wishlist;
