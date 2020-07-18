c2074bb86059e46656a223b079c7fed94cecda38
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-19 03:25:55 +0530

    + normal bag component

---
+++ src/components/shoppingBag/normalBag.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NormalBag = () => {
+  return (
+    <div className="shopping">
+      <div> this ob bag</div>
+    </div>
+  );
+};
+
+export default NormalBag;
