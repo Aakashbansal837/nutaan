7cf39989bfe9a9967770e1c8fd57dbe9ef479bc1
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-19 02:17:03 +0530

    + wishlist page

---
+++ src/pages/wishlist.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class Wishlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="/logo512.png" className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  loading: state.app.apiLoading,
+});
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wishlist);
