b9d9d74e47d7110a7a3a91a7375d5d85d8a9fba8
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-19 06:17:16 +0530

    + dummy data

--- src/pages/shoppingBag.js
+++ src/pages/shoppingBag.js
@@ -3,18 +3,41 @@ import { connect } from "react-redux";
 import NormalBag from "../components/shoppingBag/normalBag";
 import Navbar from "../components/common/navbar";
 import Footer from "../components/common/footer";
+import Img7 from "../images/Wishlist/7.png";
+import Img4 from "../images/Wishlist/4.png";
 
 class ShoppingBag extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: [
+        {
+          name: "Rust Orange",
+          img: Img7,
+          disc: "Women Pink Handblock Bird Print Straight Kurta",
+          size: "M",
+          qty: "1",
+          price: "₹ 1799",
+          prevP: "₹ 1999",
+        },
+        {
+          name: "Rust Orange",
+          img: Img4,
+          disc: "Women Pink Handblock Bird Print Straight Kurta",
+          size: "M",
+          qty: "1",
+          price: "₹ 1799",
+          prevP: "₹ 1999",
+        },
+      ],
+    };
   }
 
   render() {
     return (
       <div>
         <Navbar />
-        <NormalBag data={this.state} />
+        <NormalBag items={this.state.data} />
         <Footer />
       </div>
     );
