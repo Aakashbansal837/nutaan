f2e67de953f6b8b138397885f3d50167a4b3739e
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-19 06:18:15 +0530

    + normal shopping bag

--- src/components/shoppingBag/normalBag.js
+++ src/components/shoppingBag/normalBag.js
@@ -1,9 +1,23 @@
 import React from "react";
+import NormalCard from "./normalCard";
 
-const NormalBag = () => {
+const NormalBag = ({ items }) => {
+  var [bag, changeBag] = React.useState(items);
   return (
     <div className="shopping">
-      <div> this ob bag</div>
+      <div className="container px-0">
+        <div className="shopping-heading">Shopping Bag</div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 px-0">
+            <div className="column">
+              {bag.map((item) => {
+                return <NormalCard data={item} />;
+              })}
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>
+        </div>
+      </div>
     </div>
   );
 };
