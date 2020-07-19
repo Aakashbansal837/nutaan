c5c81d7dd7eb0de89b75043b296bd887b375e1fc
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-19 06:17:32 +0530

    card updated

--- src/components/wishlist/wishlistCard.js
+++ src/components/wishlist/wishlistCard.js
@@ -1,24 +1,20 @@
 import React from "react";
 import { ReactComponent as Close } from "../../images/close.svg";
 
-const WishlistCard = (props) => {
+const WishlistCard = ({ item, RemoveItem }) => {
   return (
     <div className="wishlist-card">
       <div className="card">
         <Close
-          onClick={() => props.RemoveItem(props.item)}
+          onClick={() => RemoveItem(item)}
           className="wishlist-card-close"
         />
-        <img
-          className="wishlist-card-img"
-          src={props.item.img}
-          alt="wishlist card"
-        />
+        <img className="wishlist-card-img" src={item.img} alt="wishlist card" />
 
         <div className="card-body wishlist-card-body">
-          <p className="card-title wishlist-card-title">{props.item.name}</p>
-          <p className="card-text wishlist-card-text">{props.item.disc}</p>
-          <p className="card-text wishlist-card-title">{props.item.price}</p>
+          <p className="card-title wishlist-card-title">{item.name}</p>
+          <p className="card-text wishlist-card-text">{item.disc}</p>
+          <p className="card-text wishlist-card-title">{item.price}</p>
         </div>
         <utton className="btn btn-outlined wishlist-card-button">
           Add to Bag
