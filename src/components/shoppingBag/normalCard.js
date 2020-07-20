a489d98c604e31b53197041e4639d024ef711597
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-20 22:25:35 +0530

    card component updates

--- src/components/shoppingBag/normalCard.js
+++ src/components/shoppingBag/normalCard.js
@@ -4,49 +4,59 @@ import { Button } from "@material-ui/core";
 const NormalCard = ({ data }) => {
   return (
     <div className="shopping-normal">
-      <div className="container px-0">
+      <div className="container">
         <div className="row">
-          <div className="col-3 px-0">
+          <div className="col-4 col-sm-3 col-md-3 col-lg-3 px-0">
             <img className="shopping-normal-img" src={data.img} alt="item" />
           </div>
-          <div className="col-8 px-0">
+          <div className="col-8 col-sm-8 col-md-9 col-lg-9">
             <div className="shopping-normal-data">
-              <div className="row">
-                <div className="col-xs-12 col-sm-12 col-md-7 col-lg-7">
-                  <div className="shopping-normal-data-heading">
-                    {data.name}
-                  </div>
-                  <div className="shopping-normal-data-disc">{data.disc}</div>
-                  <div className="shopping-normal-data-choice">
-                    <div className="shopping-normal-data-choice-txt">
-                      Size: {data.size}
+              <div className="container">
+                <div className="row">
+                  <div className="col-12 col-sm-12 col-md-8 col-lg-8">
+                    <div className="shopping-normal-data-heading">
+                      {data.name}
                     </div>
-                    <div className="shopping-normal-data-choice-txt">
-                      Qty: {data.qty}
+                    <div className="shopping-normal-data-disc">{data.disc}</div>
+                    <div className="shopping-normal-data-choice">
+                      <div className="shopping-normal-data-choice-txt">
+                        Size: {data.size}
+                      </div>
+                      <div className="shopping-normal-data-choice-txt">
+                        Qty: {data.qty}
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5 px-0">
-                  <div className="shopping-normal-data-price ">
-                    {data.price}
+                  <div className="col-12 col-sm-12 col-md-4 col-lg-4 ">
+                    <div className="shopping-normal-data-prc">
+                      <div className="shopping-normal-data-price ">
+                        {data.price}
+                      </div>
+                      <div className="shopping-normal-data-prevP">
+                        was &nbsp;{data.prevP}
+                      </div>
+                    </div>
                   </div>
-                  <div className="shopping-normal-data-prevP">
-                    was &nbsp;{data.prevP}
+                  <div className="col-12 ">
+                    <div className="shopping-divider" />
                   </div>
-                </div>
-                <div className="col-12 pr-0">
-                  <div className="shopping-divider" />
-                </div>
-                <div className="col-6 col-md-4 col-lg-4 pr-0">
-                  <Button className="shopping-btn-move">
-                    MOVE TO WISHLIST
-                  </Button>
-                </div>
-                <div className="col-6 col-md-2 col-lg-2 px-0">
-                  <Button className="shopping-btn-remove">Remove</Button>
+                  <div className="d-none d-md-block col-md-7 col-lg-7 pr-0">
+                    <Button className="shopping-btn-move">
+                      MOVE TO WISHLIST
+                    </Button>
+                  </div>
+                  <div className="d-none d-md-block col-md-5 col-lg-5 pl-0">
+                    <Button className="shopping-btn-remove">Remove</Button>
+                  </div>
                 </div>
               </div>
             </div>
+          </div>
+          <div className="col-6 d-block d-md-none px-0">
+            <Button className="shopping-btn-move">MOVE TO WISHLIST</Button>
+          </div>
+          <div className="col-6 d-block d-md-none px-0">
+            <Button className="shopping-btn-remove">Remove</Button>
           </div>
         </div>
       </div>
