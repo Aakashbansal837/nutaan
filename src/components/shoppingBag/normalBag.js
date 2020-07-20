3959fb3e48e74189f83479f87e7d0999d1fcb520
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-20 22:26:12 +0530

    col-xs-* replaced by col-*

--- src/components/shoppingBag/normalBag.js
+++ src/components/shoppingBag/normalBag.js
@@ -6,17 +6,17 @@ const NormalBag = ({ items }) => {
   var [bag, changeBag] = React.useState(items);
   return (
     <div className="shopping">
-      <div className="container px-0">
+      <div className="container">
         <div className="shopping-heading">Shopping Bag</div>
         <div className="row">
-          <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 px-0">
+          <div className="col-12 col-sm-12 col-md-8 col-lg-8">
             <div className="column">
               {bag.map((item) => {
                 return <NormalCard data={item} />;
               })}
             </div>
           </div>
-          <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+          <div className="col-12 col-sm-12 col-md-4 col-lg-4">
             <BillingCard />
           </div>
         </div>
