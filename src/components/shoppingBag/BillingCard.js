dbd40564a69597339d8c67bcb40ef1ec7b698582
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-24 23:39:34 +0530

    ^ md screen data

--- src/components/shoppingBag/BillingCard.js
+++ src/components/shoppingBag/BillingCard.js
@@ -30,37 +30,37 @@ const NormalCard = ({ item }) => {
                 <div className="col-12">
                   <div className="bill-head">Sub Total</div>
                 </div>
-                <div className="col-8">
+                <div className="col-8 pr-0">
                   <div className="bill-data-txt">Base Amount</div>
                 </div>
-                <div className="col-4">
+                <div className="col-4 pl-0">
                   <div className="bill-data-amt">₹ 3598</div>
                 </div>
-                <div className="col-8">
+                <div className="col-8 pr-0">
                   <div className="bill-data-txt">Discounts</div>
                 </div>
-                <div className="col-4">
+                <div className="col-4 pl-0">
                   <div className="bill-data-amt">- ₹ 299</div>
                 </div>
-                <div className="col-8">
+                <div className="col-8 pr-0">
                   <div className="bill-data-txt">Shipping Fee</div>
                 </div>
-                <div className="col-4">
+                <div className="col-4 pl-0">
                   <div className="bill-data-amt">₹ 100</div>
                 </div>
-                <div className="col-8">
+                <div className="col-8 pr-0">
                   <div className="bill-data-txt">Tailoring Adjustments</div>
                 </div>
-                <div className="col-4">
+                <div className="col-4 pl-0">
                   <div className="bill-data-amt">₹ 150</div>
                 </div>
                 <div className="col-12">
                   <div className="shopping-divider-2" />
                 </div>
-                <div className="col-8">
+                <div className="col-8 pr-0">
                   <div className="bill-data-txt-dark">Amount Payable</div>
                 </div>
-                <div className="col-4">
+                <div className="col-4 pl-0">
                   <div className="bill-data-amt-dark">₹ 3598</div>
                 </div>
                 <div className="col-12">
