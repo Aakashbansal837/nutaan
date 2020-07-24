71dd1a9dc8635904fdf8c5d7930663ad9ec32e6f
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-24 23:39:11 +0530

    + shopping banner

---
+++ src/components/shoppingBag/shoppingBanner.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ShoppingBanner = ({ item }) => {
+  return (
+    <div>
+      <h1>Combo card goes here</h1>
+    </div>
+  );
+};
+export default ShoppingBanner;
