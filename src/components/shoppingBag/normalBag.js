831db8d96f4f4101a47b2ba78b87b2ff62cb94e4
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-27 01:44:19 +0530

    + value , fnc imports

--- src/components/shoppingBag/normalBag.js
+++ src/components/shoppingBag/normalBag.js
@@ -2,22 +2,40 @@ import React from "react";
 import NormalCard from "./normalCard";
 import BillingCard from "./BillingCard";
 import ShoppingBanner from "./shoppingBanner";
-const NormalBag = ({ items }) => {
+import AddressCard from "./AddressCard";
+const NormalBag = ({
+  items,
+  ChangeOrderClicked,
+  PlaceOrderClicked,
+  OrderPlaced,
+}) => {
   var [bag, changeBag] = React.useState(items);
   return (
     <div className="shopping">
       <div className="container">
-        <div className="shopping-heading">Shopping Bag</div>
+        <div className="shopping-heading">
+          {OrderPlaced ? "Complete your Purchase" : "Shopping Bag"}
+        </div>
         <div className="row">
           <div className="col-12 col-sm-12 col-md-8 col-lg-8">
-            <div className="column">
-              {bag.map((item) => {
-                return <NormalCard data={item} />;
-              })}
-            </div>
+            {OrderPlaced ? (
+              <AddressCard
+                ChangeOrderClicked={() => ChangeOrderClicked()}
+                count={bag.length}
+              />
+            ) : (
+              <div className="column">
+                {bag.map((item) => {
+                  return <NormalCard data={item} />;
+                })}
+              </div>
+            )}
           </div>
           <div className="col-12 col-sm-12 col-md-4 col-lg-4">
-            <BillingCard />
+            <BillingCard
+              PlaceOrderClicked={() => PlaceOrderClicked()}
+              OrderPlaced={OrderPlaced}
+            />
           </div>
           <div className="col-12 ">
             <ShoppingBanner />
