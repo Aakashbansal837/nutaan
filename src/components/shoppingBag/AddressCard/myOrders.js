57ff7fdae2af3768c87023590fa3d97ed2d513a7
aakashbansal837 <aakashbansal837@gmail.com>
2020-07-27 05:49:00 +0530

    ^ media query adjustments

--- src/components/shoppingBag/AddressCard/myOrders.js
+++ src/components/shoppingBag/AddressCard/myOrders.js
@@ -6,10 +6,10 @@ const MyOrders = ({ count, ChangeOrderClicked }) => {
     <div className="shopping-addr-ordr">
       <div className="container">
         <div className="row">
-          <div className="col-1">
+          <div className="col-2 col-sm-2 col-md-2 col-lg-1">
             <div className="shopping-addr-ordr-nmbr">1</div>
           </div>
-          <div className="col-8">
+          <div className="col-6 col-sm-7 col-md-7 col-lg-8">
             <div className="shopping-addr-ordr-main">
               <span className="shopping-addr-ordr-main-head">My Orders</span>
               <br />
@@ -18,7 +18,7 @@ const MyOrders = ({ count, ChangeOrderClicked }) => {
               </span>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-4 col-sm-3 col-md-3 col-lg-3">
             <div className="shopping-addr-ordr-btn">
               <Button
                 className="shopping-addr-ordr-btn-1"
