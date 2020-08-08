5c96853c59e46e00978842e51930bd0309187957
aakashbansal837 <aakashbansal837@gmail.com>
2020-08-09 02:44:57 +0530

    + product details data

--- src/pages/productDetails.js
+++ src/pages/productDetails.js
@@ -3,14 +3,27 @@ import Product from "../components/productDetails";
 import Navbar from "../components/common/navbar";
 import Footer from "../components/common/footer";
 
-const ProductDetails = () => {
-  return (
-    <div>
-      <Navbar />
-      <Product />
-      <Footer />
-    </div>
-  );
-};
+class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: {
+        name: "Rust Orange",
+        disc: "Women Pink Handblock Bird Print Straight Kurta",
+        price: "₹ 1699",
+        prevP: "₹ 2199",
+      },
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Product item={this.state.item} />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default ProductDetails;
