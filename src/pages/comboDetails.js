2b543a177616212f89575a752f8c14efcf18e5b6
aakashbansal837 <aakashbansal837@gmail.com>
2020-08-10 04:46:25 +0530

    + combo details page

---
+++ src/pages/comboDetails.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import Product from "../components/productDetails";
+import Navbar from "../components/common/navbar";
+import Footer from "../components/common/footer";
+
+class ComboDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: {
+        name: "Rust Orange",
+        disc: "Women Pink Handblock Bird Print Straight Kurta",
+        price: "₹ 1699",
+        prevP: "₹ 2199",
+        neck: "Round Neck",
+        shape: "Straight",
+        length: "Calf Length",
+        sleeve: "No Sleeves",
+        slit: "Sleeves",
+        print: "Ethnic Motifs",
+        occasion: "Daily",
+        material: "Machine Wash",
+        season: "Summer",
+        fit: "Skinny",
+        color: [
+          "#FF6060",
+          "#77E0C9",
+          "#79CCEC",
+          "#FF9956",
+          "#FF9956",
+          "#FF9956",
+          "#FF9956",
+        ],
+        size: [
+          ["s", "5"],
+          ["m", "2"],
+          ["l", "0"],
+          ["xl", "10"],
+        ],
+      },
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Product item={this.state.item} />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  loading: state.app.apiLoading,
+});
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ComboDetails);
