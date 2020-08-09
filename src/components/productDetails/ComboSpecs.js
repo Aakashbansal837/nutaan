597f73984db52dd8ef6b5c66da2bef118eb1d95a
aakashbansal837 <aakashbansal837@gmail.com>
2020-08-10 05:25:29 +0530

    + combo specs

---
+++ src/components/productDetails/ComboSpecs.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ComboSpecs = ({ data }) => {
+  return (
+    <div className="product-detail-spec">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 px-0">
+            <div className="product-detail-spec-header">Products Included</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ComboSpecs;
