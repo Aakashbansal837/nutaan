7267bc235fd7d6635f02ada285a35938b2f853df
aakashbansal837 <aakashbansal837@gmail.com>
2020-08-11 02:24:11 +0530

    + three item corousel component

--- src/components/Corousal/ThreeItem.js
+++ src/components/Corousal/ThreeItem.js
@@ -1,7 +1,39 @@
 import React from "react";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const ThreeItemCorousal = ({ images }) => {
-  return <div className="threeItemCorousal"> Three item Cosousal</div>;
+  return (
+    <OwlCarousel
+      // className="threeItemCorousal"
+      loop
+      margin={10}
+      nav
+      responsiveClass="true"
+      autoplay="true"
+      autoplayTimeout="2000"
+      responsive={{
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        768: {
+          items: 3,
+        },
+      }}
+    >
+      {images.map((img) => {
+        return (
+          <div class="item">
+            <img src={img} alt="product detail" />
+          </div>
+        );
+      })}
+    </OwlCarousel>
+  );
 };
 
 export default ThreeItemCorousal;
