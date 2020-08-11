96c56f670856c3dc840dbf8933a67580cdad00b2
aakashbansal837 <aakashbansal837@gmail.com>
2020-08-12 02:33:47 +0530

    + profile

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+const ProfileOptions = () => {
+  return <div></div>;
+};
+
 const Profile = () => {
-  return <div className="profile">User Profile</div>;
+  return (
+    <div className="profile">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 ">
+            <div className="profile-header">My Profile</div>
+          </div>
+          <div className="col-12">
+            <div className="profile-main">
+              <div className="container">
+                <div className="row">
+                  <div className="col-3 px-0">
+                    <ProfileOptions />
+                  </div>
+                  <div className="col-9 "></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
