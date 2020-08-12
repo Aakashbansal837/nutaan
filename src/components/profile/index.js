16186fc6e6cad195b6b6f35151e7cf94e037784b
aakashbansal837 <aakashbansal837@gmail.com>
2020-08-13 05:16:15 +0530

    + new state elements

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -7,7 +7,12 @@ import PaymentDetails from "./paymentDetails";
 import PersonalInfo from "./PersonalInfo";
 
 const Profile = () => {
-  const [active, setActive] = React.useState(6);
+  const [active, setActive] = React.useState(1);
+  const [option, setOption] = React.useState(0);
+  const SelectionChanged = (option) => {
+    setActive(option);
+    setOption(1);
+  };
   return (
     <div className="profile">
       <div className="container">
@@ -19,15 +24,39 @@ const Profile = () => {
             <div className="profile-main">
               <div className="container">
                 <div className="row">
-                  <div className="col-12 col-sm-12 col-md-3 col-lg-3 px-0">
+                  <div
+                    className={
+                      option == 0
+                        ? "col-12 col-sm-12 col-md-3 col-lg-3 px-0"
+                        : "d-none d-md-block  col-md-3 col-lg-3 px-0"
+                    }
+                  >
                     <ProfileOptions
                       active={active}
-                      setActive={(value) => {
-                        setActive(value);
+                      changeOption={(value) => {
+                        SelectionChanged(value);
                       }}
                     />
                   </div>
                   <div className="d-none d-md-block col-9 ">
+                    {active == 1 ? (
+                      <MyAccount />
+                    ) : active == 3 ? (
+                      <MyOrder />
+                    ) : active == 4 ? (
+                      <Addresses />
+                    ) : active == 5 ? (
+                      <PaymentDetails />
+                    ) : active == 6 ? (
+                      <PersonalInfo />
+                    ) : null}
+                  </div>
+
+                  <div
+                    className={
+                      option == 1 ? "d-block d-md-none" : "d-none col-12"
+                    }
+                  >
                     {active == 1 ? (
                       <MyAccount />
                     ) : active == 3 ? (
