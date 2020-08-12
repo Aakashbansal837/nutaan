7905c41f9b92fe6c38f9deafe3b64b490e8ad295
aakashbansal837 <aakashbansal837@gmail.com>
2020-08-13 03:39:30 +0530

    +profile payment component

--- src/components/profile/paymentDetails.js
+++ src/components/profile/paymentDetails.js
@@ -1,13 +1,80 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
 const PaymentDetails = () => {
+  const CardData = [
+    {
+      name: "Abbie Wilson",
+      bank: "HSBC Debit Card",
+      cardNumber: "2827 xxxx xxxx 8901",
+      validity: "09/25",
+    },
+    {
+      name: "Abbie Wilson",
+      bank: "UBS Credit Card",
+      cardNumber: "2827 xxxx xxxx 8901",
+      validity: "09/25",
+    },
+  ];
   return (
-    <div className="profile-account">
+    <div className="profile-payment">
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <div className="profile-account-head"></div>
+            <div className="profile-payment-head">
+              <span className="profile-payment-head-text">
+                Manage your saved Cards or add a new one
+              </span>
+              <Button
+                variant="contained"
+                color="secondary"
+                className="profile-payment-head-btn"
+              >
+                Add a Card
+              </Button>
+            </div>
           </div>
+
+          {CardData.map((dt) => (
+            <div className="col-6">
+              <div className="profile-payment-main">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-6">
+                      <div className="profile-payment-main-title">NAME</div>
+                      <div className="profile-payment-main-data">{dt.name}</div>
+                    </div>
+                    <div className="col-6">
+                      <div className="profile-payment-main-title-hidden">
+                        bank
+                      </div>
+                      <div className="profile-payment-main-data">{dt.bank}</div>
+                    </div>
+                    <div className="col-6">
+                      <div className="profile-payment-main-title">
+                        CARD NUMBER
+                      </div>
+                      <div className="profile-payment-main-data">
+                        {dt.cardNumber}
+                      </div>
+                    </div>
+                    <div className="col-6">
+                      <div className="profile-payment-main-title">VALIDITY</div>
+                      <div className="profile-payment-main-data">
+                        {dt.validity}
+                      </div>
+                    </div>
+                    <div className="col-12 px-0">
+                      <div className="profile-payment-main-option">
+                        <span>Edit</span>
+                        <span>Remove</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
