69ae09c81a438f669ad7bd8cec10c34ef81b718a
aakashbansal837 <aakashbansal837@gmail.com>
2020-08-22 17:38:46 +0530

    + mouse hover and leave event

--- src/components/Corousal/ThreeItem.js
+++ src/components/Corousal/ThreeItem.js
@@ -4,40 +4,47 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const ThreeItemCorousal = ({ images }) => {
+  const [show, setShow] = React.useState(false);
   return (
-    <OwlCarousel
-      className="threeItemCorousal"
-      loop
-      margin={10}
-      responsiveClass="true"
-      autoplay="true"
-      autoplayTimeout="7000"
-      nav
-      dots={true}
-      navText={[
-        `<div class='three-nav-btn prev-slide'><i class="fa fa-chevron-left" aria-hidden="true"></i></div>`,
-        `<div class='three-nav-btn next-slide'><i class="fa fa-chevron-right" aria-hidden="true"></i></div>`,
-      ]}
-      responsive={{
-        0: {
-          items: 1,
-        },
-        600: {
-          items: 2,
-        },
-        768: {
-          items: 3,
-        },
-      }}
-    >
-      {images.map((img) => {
-        return (
-          <div class="item">
-            <img src={img} alt="product detail" />
-          </div>
-        );
-      })}
-    </OwlCarousel>
+    <div onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+      <OwlCarousel
+        className="threeItemCorousal"
+        loop
+        margin={10}
+        responsiveClass="true"
+        autoplay="true"
+        autoplayTimeout="7000"
+        nav
+        dots={true}
+        navText={[
+          `<div class='three-nav-btn prev-slide' style="display : ${
+            show ? "block" : "none"
+          }"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>`,
+          `<div class='three-nav-btn next-slide' style="display : ${
+            show ? "block" : "none"
+          }"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>`,
+        ]}
+        responsive={{
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 2,
+          },
+          768: {
+            items: 3,
+          },
+        }}
+      >
+        {images.map((img) => {
+          return (
+            <div class="item">
+              <img src={img} alt="product detail" />
+            </div>
+          );
+        })}
+      </OwlCarousel>
+    </div>
   );
 };
 
