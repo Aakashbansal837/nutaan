de2798d31e5712804f682a3df63a153824b8b90c
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-26 21:22:36 +0530

    + similar products

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,18 +3,73 @@ import { connect } from "react-redux";
 import Navbar from "../components/common/navbar";
 import Footer from "../components/common/footer";
 import HomepageComponent from "../components/Homepage";
+import { imageArray } from "../constants";
 
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      item: {
+        similarProduct: [
+          {
+            name: "BIBA",
+            disc: "Kurta with Palazzos and Dupatta",
+            img:
+              imageArray[Math.floor(Math.random() * (imageArray.length - 1))],
+            price: "₹ 1,899",
+          },
+          {
+            name: "BIBA",
+            disc: "Kurta with Palazzos and Dupatta",
+            img:
+              imageArray[Math.floor(Math.random() * (imageArray.length - 1))],
+            price: "₹ 1,899",
+          },
+          {
+            name: "BIBA",
+            disc: "Kurta with Palazzos and Dupatta",
+            img:
+              imageArray[Math.floor(Math.random() * (imageArray.length - 1))],
+            price: "₹ 1,899",
+          },
+          {
+            name: "BIBA",
+            disc: "Kurta with Palazzos and Dupatta",
+            img:
+              imageArray[Math.floor(Math.random() * (imageArray.length - 1))],
+            price: "₹ 1,899",
+          },
+          {
+            name: "BIBA",
+            disc: "Kurta with Palazzos and Dupatta",
+            img:
+              imageArray[Math.floor(Math.random() * (imageArray.length - 1))],
+            price: "₹ 1,899",
+          },
+          {
+            name: "BIBA",
+            disc: "Kurta with Palazzos and Dupatta",
+            img:
+              imageArray[Math.floor(Math.random() * (imageArray.length - 1))],
+            price: "₹ 1,899",
+          },
+          {
+            name: "BIBA",
+            disc: "Kurta with Palazzos and Dupatta",
+            img:
+              imageArray[Math.floor(Math.random() * (imageArray.length - 1))],
+            price: "₹ 1,899",
+          },
+        ],
+      },
+    };
   }
 
   render() {
     return (
       <>
         <Navbar />
-        <HomepageComponent />
+        <HomepageComponent item={this.state.item} />
         <Footer />
       </>
     );
