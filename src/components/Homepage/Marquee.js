4cba6da4d126c4b8d2909f5d13b35c767043827f
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-26 20:07:01 +0530

    + marquee

---
+++ src/components/Homepage/Marquee.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Marquee() {
+  return (
+    <marquee className="hmpg-mrq" direction="left" scrollamount="10">
+      Delayed Delivery due to COVID-19 situation.
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      Delayed Delivery due to COVID-19 situation.
+    </marquee>
+  );
+}
