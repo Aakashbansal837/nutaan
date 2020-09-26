52432d11ce0f00f3d00afca0a6a47daff93a1078
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-26 21:23:01 +0530

    + similar products component

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Banner1 from "./Banner1";
+import Category from "./Category";
 import Marquee from "./Marquee";
+import FiveItemCorousal from "../Corousal/FiveItem";
 
-export default function Homepage() {
+const Homepage = ({ item }) => {
   return (
     <div className="hmpg">
       <Marquee />
       <Banner1 />
+      <Category />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-12 px-0">
+            <div className="product-detail-similar-product">
+              Best Selling Products
+            </div>
+          </div>
+          <div className="col-12 px-0">
+            <FiveItemCorousal items={item.similarProduct} />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Homepage;
