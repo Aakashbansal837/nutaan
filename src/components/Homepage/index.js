92b72e688e25f168b12bc6ae635014dca236de4f
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-26 20:09:00 +0530

    + homepage component

---
+++ src/components/Homepage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner1 from "./Banner1";
+import Marquee from "./Marquee";
+
+export default function Homepage() {
+  return (
+    <div className="hmpg">
+      <Marquee />
+      <Banner1 />
+    </div>
+  );
+}
