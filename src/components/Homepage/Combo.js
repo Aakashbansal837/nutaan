bf30094cd1bf44987ede511dc1394f9f41deb765
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-27 15:21:48 +0530

    + homepage combo

---
+++ src/components/Homepage/Combo.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { homepageCombo } from "../../constants";
+import { Button } from "@material-ui/core";
+
+const Combo = () => {
+  return (
+    <div className="hmpg-combo">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="hmpg-combo-head">Combos / Popular Sub-category</div>
+          </div>
+
+          {homepageCombo.map((img) => {
+            return (
+              <div className="col-4">
+                <div className="hmpg-combo-main">
+                  <img src={img} className="hmpg-combo-main-img" />
+                  <center className="hmpg-combo-main-btn">
+                    <Button
+                      className="hmpg-combo-main-btn-button"
+                      color="secondary"
+                      variant="contained"
+                    >
+                      View Combo
+                    </Button>
+                  </center>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Combo;
