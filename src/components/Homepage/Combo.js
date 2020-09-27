1d0a2c0247fe8dc003a8f2c966f3ec0a615e7aa3
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-27 20:24:28 +0530

    + small screen changes

--- src/components/Homepage/Combo.js
+++ src/components/Homepage/Combo.js
@@ -15,8 +15,11 @@ const Combo = () => {
             return (
               <div className="col-12 col-sm-12 col-md-6 col-lg-4">
                 <div className="hmpg-combo-main">
-                  <img src={img} className="hmpg-combo-main-img" />
-                  <center className="hmpg-combo-main-btn">
+                  <img
+                    src={img}
+                    className="hmpg-combo-main-img d-none d-md-block"
+                  />
+                  <center className="hmpg-combo-main-btn d-none d-md-block">
                     <Button
                       className="hmpg-combo-main-btn-button"
                       color="secondary"
@@ -25,6 +28,16 @@ const Combo = () => {
                       View Combo
                     </Button>
                   </center>
+                  <div class="content d-block d-md-none">
+                    <a href="#">
+                      <div className="content-overlay"></div>
+                      <img className="hmpg-combo-main-img" src={img} />
+                      <div className="content-details fadeIn-bottom">
+                        <h3 className="content-title">Collection</h3>
+                        <p className="content-text">most amazing thing</p>
+                      </div>
+                    </a>
+                  </div>
                 </div>
               </div>
             );
