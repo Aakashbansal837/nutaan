3c23ff1ef841cb78d54356eda7a02d1de63cb1e8
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-27 19:42:10 +0530

    homepage added

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fbcc6d6a5aaa3bb9ef2377adc6a92fd.js"
+  "/precache-manifest.95d5a5440b401b0983da304aadf5bd9b.js"
 );
 
 self.addEventListener('message', (event) => {
