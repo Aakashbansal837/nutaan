cbd44665c0625184fb09e182d22889ecda914e60
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-27 19:26:18 +0530

    + media changes

--- src/components/Homepage/Truefit.js
+++ src/components/Homepage/Truefit.js
@@ -5,11 +5,11 @@ const Truefit = () => {
     <div className="hmpg-truefit">
       <div className="container">
         <div className="row">
-          <div className="col-5">
+          <div className="col-12 col-sm-12 col-md-5 col-lg-5">
             <div className="hmpg-truefit-txt-1">Introducing</div>
             <div className="hmpg-truefit-txt-2">Nutaan Custom Fit</div>
           </div>
-          <div className="col-7">
+          <div className="col-12 col-sm-12 col-md-7 col-lg-7">
             <div className="hmpg-truefit-txt-3">Tru-Fit Promise</div>
           </div>
         </div>
