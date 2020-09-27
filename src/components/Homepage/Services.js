f338a930e8423ea85032df93325359890f9095c6
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-27 15:51:26 +0530

    + small screen services

--- src/components/Homepage/Services.js
+++ src/components/Homepage/Services.js
@@ -5,20 +5,20 @@ const Services = () => {
     <div className="hmpg-service">
       <div className="container">
         <div className="row">
-          <div className="col-4 ">
+          <div className="col-4 col-sm-4 col-md-4 col-lg-4 ">
             <div className="container">
               <div className="row">
-                <div className="col-3">
+                <div className="col-4">
                   <div className="hmpg-service-icon">
                     <i class="fa fa-scissors" aria-hidden="true"></i>
                   </div>
                 </div>
-                <div className="col-8">
+                <div className="col-8 pr-0">
                   <div className="hmpg-service-main">
                     <div className="hmpg-service-head">
                       Fitting At Your Home
                     </div>
-                    <div className="hmpg-service-disc">
+                    <div className="d-none d-md-block hmpg-service-disc">
                       Clothes customised to you
                     </div>
                   </div>
@@ -27,7 +27,7 @@ const Services = () => {
             </div>
           </div>
 
-          <div className="col-4 ">
+          <div className="col-4 col-sm-4 col-md-4 col-lg-4 ">
             <div className="container">
               <div className="row">
                 <div className="col-3">
@@ -35,12 +35,12 @@ const Services = () => {
                     <i class="fa fa-smile-o" aria-hidden="true"></i>
                   </div>
                 </div>
-                <div className="col-9">
+                <div className="col-8 pr-0">
                   <div className="hmpg-service-main">
                     <div className="hmpg-service-head">
                       Consult Our Designers
                     </div>
-                    <div className="hmpg-service-disc">
+                    <div className="d-none d-md-block hmpg-service-disc">
                       Helping you decide what to wear
                     </div>
                   </div>
@@ -49,7 +49,7 @@ const Services = () => {
             </div>
           </div>
 
-          <div className="col-4 ">
+          <div className="col-4 col-sm-4 col-md-4 col-lg-4 ">
             <div className="container">
               <div className="row">
                 <div className="col-3">
@@ -57,10 +57,10 @@ const Services = () => {
                     <i class="fa fa-home" aria-hidden="true"></i>
                   </div>
                 </div>
-                <div className="col-8">
+                <div className="col-8 pr-0">
                   <div className="hmpg-service-main">
                     <div className="hmpg-service-head">Doorstep Services</div>
-                    <div className="hmpg-service-disc">
+                    <div className="d-none d-md-block hmpg-service-disc">
                       Bringing exciting stuff to you
                     </div>
                   </div>
