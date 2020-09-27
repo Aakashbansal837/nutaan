6b9ce99571c620342db845ab3758308c6943235c
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-27 13:54:37 +0530

    +service bootstrap

--- src/components/Homepage/Services.js
+++ src/components/Homepage/Services.js
@@ -3,62 +3,66 @@ import React from "react";
 const Services = () => {
   return (
     <div className="hmpg-service">
-      {/* <div className="container"> */}
-      <div className="row">
-        <div className="col-4 ">
-          <div className="container">
-            <div className="row">
-              <div className="col-1"></div>
-              <div className="col-3">
-                <div className="hmpg-service-icon">
-                  <i class="fa fa-scissors" aria-hidden="true"></i>
+      <div className="container">
+        <div className="row">
+          <div className="col-4 ">
+            <div className="container">
+              <div className="row">
+                <div className="col-3">
+                  <div className="hmpg-service-icon">
+                    <i class="fa fa-scissors" aria-hidden="true"></i>
+                  </div>
                 </div>
-              </div>
-              <div className="col-8">
-                <div className="hmpg-service-main">
-                  <div className="hmpg-service-head">Fitting At Your Home</div>
-                  <div className="hmpg-service-disc">
-                    Clothes customised to you
+                <div className="col-8">
+                  <div className="hmpg-service-main">
+                    <div className="hmpg-service-head">
+                      Fitting At Your Home
+                    </div>
+                    <div className="hmpg-service-disc">
+                      Clothes customised to you
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="col-4 ">
-          <div className="container">
-            <div className="row">
-              <div className="col-3">
-                <div className="hmpg-service-icon">
-                  <i class="fa fa-smile-o" aria-hidden="true"></i>
+          <div className="col-4 ">
+            <div className="container">
+              <div className="row">
+                <div className="col-3">
+                  <div className="hmpg-service-icon">
+                    <i class="fa fa-smile-o" aria-hidden="true"></i>
+                  </div>
                 </div>
-              </div>
-              <div className="col-8">
-                <div className="hmpg-service-main">
-                  <div className="hmpg-service-head">Consult Our Designers</div>
-                  <div className="hmpg-service-disc">
-                    Helping you decide what to wear
+                <div className="col-9">
+                  <div className="hmpg-service-main">
+                    <div className="hmpg-service-head">
+                      Consult Our Designers
+                    </div>
+                    <div className="hmpg-service-disc">
+                      Helping you decide what to wear
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="col-4 ">
-          <div className="container">
-            <div className="row">
-              <div className="col-3">
-                <div className="hmpg-service-icon">
-                  <i class="fa fa-home" aria-hidden="true"></i>
+          <div className="col-4 ">
+            <div className="container">
+              <div className="row">
+                <div className="col-3">
+                  <div className="hmpg-service-icon">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                  </div>
                 </div>
-              </div>
-              <div className="col-8">
-                <div className="hmpg-service-main">
-                  <div className="hmpg-service-head">Doorstep Services</div>
-                  <div className="hmpg-service-disc">
-                    Bringing exciting stuff to you
+                <div className="col-8">
+                  <div className="hmpg-service-main">
+                    <div className="hmpg-service-head">Doorstep Services</div>
+                    <div className="hmpg-service-disc">
+                      Bringing exciting stuff to you
+                    </div>
                   </div>
                 </div>
               </div>
@@ -66,7 +70,6 @@ const Services = () => {
           </div>
         </div>
       </div>
-      {/* </div> */}
     </div>
   );
 };
