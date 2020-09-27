cfc9345000d1472504bad411eac2ca411425b530
aakashbansal837 <aakashbansal837@gmail.com>
2020-09-27 11:27:56 +0530

    + homepage updates

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,24 +1,40 @@
 import React from "react";
-import Banner1 from "./Banner1";
 import Category from "./Category";
 import Marquee from "./Marquee";
 import FiveItemCorousal from "../Corousal/FiveItem";
+import { ReactComponent as HomepageBanner } from "../../images/homepage-banner.svg";
+import Collection from "./Collection";
 
 const Homepage = ({ item }) => {
   return (
     <div className="hmpg">
       <Marquee />
-      <Banner1 />
+      {/* banner top here */}
+      <div className="hmpg-bnnr-1">
+        <a href="/product-details">
+          <HomepageBanner className="hmpg-bnnr-1-img" />
+        </a>
+      </div>
       <Category />
+
+      {/* banner 2 here */}
+      <center className="hmpg-bnnr-2">
+        <a href="/combo-details">
+          <HomepageBanner className="hmpg-bnnr-2-img" />
+        </a>
+      </center>
+
+      {/* banner 3 here */}
+      <Collection />
 
       <div className="container">
         <div className="row">
-          <div className="col-12 px-0">
+          <div className="col-12">
             <div className="product-detail-similar-product">
               Best Selling Products
             </div>
           </div>
-          <div className="col-12 px-0">
+          <div className="col-12">
             <FiveItemCorousal items={item.similarProduct} />
           </div>
         </div>
